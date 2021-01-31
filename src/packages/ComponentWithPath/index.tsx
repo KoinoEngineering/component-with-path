62e8f93f26994cf126073517c610229fce46889a
koinoengineering <email@koino.engineering>
2021-01-31 11:58:33 +0000

    Changed not to use consumer

--- src/packages/ComponentWithPath/index.tsx
+++ src/packages/ComponentWithPath/index.tsx
@@ -4,9 +4,9 @@ export interface CreateRootOptions {
   separator?: string
 }
 
-const defaultOptions: Required<CreateRootOptions> = {
+const defaultOptions: Required<CreateRootOptions> = Object.freeze({
   separator: "."
-};
+});
 
 export const createRoot = (rootPath: string, options?: CreateRootOptions) => {
   const { separator } = {
@@ -17,15 +17,16 @@ export const createRoot = (rootPath: string, options?: CreateRootOptions) => {
   const usePath = () => { return useContext(Context); };
   const withPath = function <P extends WithPathProps>(WrappedComponent: ComponentType<P>) {
     const ComponentWithPath: React.FC<Omit<P, "usePath">> = (props) => {
-      return <Context.Consumer>
-        {
-          path => {
-            return <Context.Provider value={path + separator + props.id}>
-              <WrappedComponent {...({ ...props, usePath } as P)} />
-            </Context.Provider>;
-          }
-        }
-      </Context.Consumer>;
+      const contextPath = useContext(Context);
+      const pathFromRoot = contextPath + separator;
+      const isStartWithRoot = () => { return props.id.startsWith(pathFromRoot); };
+      if (isStartWithRoot()) {
+        warn("idがパスで始まるのを検出しました。パスの重複を避けるためにid中のパス部分を削除してからパスに連結します。");
+      }
+      const childPath = isStartWithRoot() ? props.id.replace(pathFromRoot, "") : props.id;
+      return <Context.Provider value={pathFromRoot + childPath}>
+        <WrappedComponent {...({ ...props, usePath } as P)} />
+      </Context.Provider>;
     };
     ComponentWithPath.displayName = "ComponentWithPath(" + (WrappedComponent.displayName || WrappedComponent.name || "Component") + ")";
     return ComponentWithPath;
@@ -38,3 +39,11 @@ export interface WithPathProps {
   id: string;
   usePath: () => string
 }
+
+
+const warn = (...args: Parameters<typeof console.warn>) => {
+  if (process.env.NODE_ENV !== "production") {
+    // eslint-disable-next-line no-console
+    console.warn(...args);
+  }
+};
